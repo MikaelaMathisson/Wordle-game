7b46c6e28a4f9cf8ab166eafad22143313672678
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-03-20 18:12:00 +0100

    finishing game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
-import { createContext, useState } from "react";
-import { boardDefault } from "./Words";
-
+import { createContext, useEffect, useState } from "react";
+import { boardDefault, generateWordSet } from "./Words";
+import GameOver from "./components/GameOver";
 export const AppContext = createContext();
 
-function App() {
+export default function App() {
   const [board, setBoard] = useState(boardDefault);
   const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const [correctWord, setCorrectWord] = useState("");
+  const [gameOver, setGameOver] = useState({
+    gameOver: false,
+    guessedWord: false,
+  });
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+      setCorrectWord(words.todaysWord);
+    });
+  }, []);
 
   const onSelectLetter = (keyVal) => {
     if (currAttempt.letterPos > 4) return;
@@ -28,7 +42,26 @@ function App() {
 
   const onEnter = () => {
     if (currAttempt.letterPos !== 5) return;
-    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+
+    let currWord = "";
+    for (let i = 0; i < 5; i++) {
+      currWord += board[currAttempt.attempt][i];
+    }
+
+    if (wordSet.has(currWord.toLowerCase())) {
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+    } else {
+      alert("Word not found");
+    }
+
+    if (currWord === correctWord) {
+      setGameOver({ gameOver: true, guessedWord: true });
+      return;
+    }
+
+    if (currAttempt.attempt === 5) {
+      setGameOver({ gameOver: true, guessedWord: false });
+    }
   };
   return (
     <div className="App">
@@ -44,15 +77,18 @@ function App() {
           onSelectLetter,
           onDelete,
           onEnter,
+          correctWord,
+          setDisabledLetters,
+          disabledLetters,
+          setGameOver,
+          gameOver,
         }}
       >
         <div className="game">
           <Board />
-          <Keyboard />
+          {gameOver.gameOver ? <GameOver /> : <Keyboard />}
         </div>
       </AppContext.Provider>
     </div>
   );
 }
-
-export default App;
--- my-app/src/components/Keyboard.js
+++ my-app/src/components/Keyboard.js
@@ -3,7 +3,8 @@ import { AppContext } from "../App";
 import Key from "./Key";
 
 export default function Keyboard() {
-  const { onDelete, onEnter, onSelectLetter } = useContext(AppContext);
+  const { onDelete, onEnter, onSelectLetter, disabledLetters } =
+    useContext(AppContext);
 
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
@@ -45,20 +46,20 @@ export default function Keyboard() {
     <div className="keyboard" onKeyDown={handleKeyboard}>
       <div className="line1">
         {keys1.map((key) => {
-          return <Key keyVal={key} />;
+          return <Key keyVal={key} disabled={disabledLetters.includes(key)} />;
         })}
       </div>
       <div className="line2">
         {keys2.map((key) => {
-          return <Key keyVal={key} />;
+          return <Key keyVal={key} disabled={disabledLetters.includes(key)} />;
         })}
       </div>
       <div className="line3">
-        <Key keyVal={"Enter"} bigKey />
-        {keys3.map((key) => {
-          return <Key keyVal={key} />;
-        })}
         <Key keyVal={"Delete"} bigKey />
+        {keys3.map((key) => {
+          return <Key keyVal={key} disabled={disabledLetters.includes(key)} />;
+        })}
+        <Key keyVal={"Enter"} bigKey />
       </div>
     </div>
   );
