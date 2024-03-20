e57f3c618d3975b08f587cdbabf068b1653e47f0
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-03-20 14:34:27 +0100

    setting up letters and wordle basics

---
+++ my-app/src/components/Key.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+
+export default function Key({ keyVal, bigKey }) {
+  const { onDelete, onSelectLetter, onEnter } = useContext(AppContext);
+
+  const selectLetter = () => {
+    if (keyVal === "Enter") {
+      onEnter();
+    } else if (keyVal === "Delete") {
+      onDelete();
+    } else {
+      onSelectLetter(keyVal);
+    }
+  };
+  return (
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+      {keyVal}
+    </div>
+  );
+}
